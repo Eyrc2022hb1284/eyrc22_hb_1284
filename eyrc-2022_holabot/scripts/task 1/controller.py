5f00023c9e5ca4326489ebff15a815877ba85913
Debrup <120cs0201@nitrkl.ac.in>
2022-10-17 20:05:20 +0530

    Code ready for test case run

--- eyrc-2022_holabot/scripts/task 1/controller.py
+++ eyrc-2022_holabot/scripts/task 1/controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseArray
 from nav_msgs.msg import Odometry
 import math
 from tf.transformations import euler_from_quaternion
@@ -15,8 +15,8 @@ class goToPose:
         self.y=0
         self.theta=0
 
-        self.linear_thresh=0.05
-        self.ang_thresh=0.1
+        self.linear_thresh=0.01
+        self.ang_thresh=0.04
 
         # goals
         self.x_goals=[1, -1, -1, 1, 0]
@@ -24,19 +24,19 @@ class goToPose:
         self.theta_goals=[math.pi/4, 3*math.pi/4, -3*math.pi/4, -math.pi/4, 0]
 
         self.odom_sub=rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        # self.test_sub=rospy.Subscriber('task1_goals', PoseArray, self.task1_goals_Cb)
         self.cmd_pub=rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # pid params
-        self.params_linear={'Kp':1, 'Ki':0, 'Kd':0}
+        self.params_linear={'Kp':2, 'Ki':0, 'Kd':0}
         self.params_ang={'Kp':0.5, 'Ki':0, 'Kd':0}
 
         self.intg=0
         self.last_error=0
 
         self.msg=Twist()
-        self.rate=rospy.Rate(100)
+        self.rate=rospy.Rate(10)
 
-        # while not rospy.is_shutdown():
         for i in range(len(self.x_goals)):
             goal_x=self.x_goals[i]
             goal_y=self.y_goals[i]
@@ -45,24 +45,23 @@ class goToPose:
             while(True):
                 angle_error=goal_theta-self.theta
 
-                error_x=goal_x-self.x
-                error_y=goal_y-self.y
+                error_x=(goal_x-self.x)*math.cos(self.theta)+(goal_y-self.y)*math.sin(self.theta)
+                error_y=-(goal_x-self.x)*math.sin(self.theta)+(goal_y-self.y)*math.cos(self.theta)
 
                 v_x, v_y=self.getLinearVel(error_x,  error_y, self.params_linear)
                 ang_vel=self.getAngVel(angle_error, self.params_ang)
 
                 self.msg.linear.x=v_x
                 self.msg.linear.y=v_y
-                # self.msg.angular.z=ang_vel
+                self.msg.angular.z=ang_vel
 
                 self.cmd_pub.publish(self.msg)
                 self.rate.sleep()
 
                 # if bot reached goal, move to next goal
-                # abs(angle_error)<=self.ang_thresh and 
-                if abs(error_x)<=self.linear_thresh and abs(error_y)<=self.linear_thresh :
+                if abs(angle_error)<=self.ang_thresh and abs(error_x)<=self.linear_thresh and abs(error_y)<=self.linear_thresh :
                     self.stop(x=True, y=True, z=True)
-                    rospy.sleep(2)
+                    rospy.sleep(1)
                     break
 
         rospy.loginfo("Task finished")
@@ -78,6 +77,20 @@ class goToPose:
         self.y = data.pose.pose.position.y # y coordinate of bot
         _, _, self.theta = euler_from_quaternion([x,y,z,w]) #real time orientation of bot
 
+    def task1_goals_Cb(self, msg):
+        self.x_goals.clear()
+        self.y_goals.clear()
+        self.theta_goals.clear()
+
+        for waypoint_pose in msg.poses:
+            self.x_goals.append(waypoint_pose.position.x)
+            self.y_goals.append(waypoint_pose.position.y)
+
+            orientation_q = waypoint_pose.orientation
+            orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+            theta_goal = euler_from_quaternion (orientation_list)[2]
+            self.theta_goals.append(theta_goal)
+
     def pid(self, error, const):
         prop = error
         self.intg = error + self.intg
@@ -89,7 +102,7 @@ class goToPose:
     def getAngVel(self, error, const):
         ang_vel=0
 
-        if abs(error) > 0.1:
+        if abs(error) > self.ang_thresh:
             if error > 3.14:
                 ang_vel = self.pid((error-6.28), const)
             elif error < -3.14:
@@ -114,7 +127,7 @@ class goToPose:
         v_x=0
         v_y=0
         
-        if abs(error_x)>0.05 or abs(error_y)>0.05:
+        if abs(error_x)>self.linear_thresh or abs(error_y)>self.linear_thresh:
             v_x=self.pid(error_x, const)
             v_y=self.pid(error_y, const)
             print("vel_x: {} vel_y: {}".format(v_x, v_y))

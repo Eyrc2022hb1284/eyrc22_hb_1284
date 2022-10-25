1302a01152bbd4d5b49b8ab7d9eb9ce01ff4250e
Your Name <aman9.rourkela.com>
2022-10-26 01:33:27 +0530

    progress+wrench not working idk

--- eyrc-2022_holabot/scripts/task 2/controller.py
+++ eyrc-2022_holabot/scripts/task 2/controller.py
@@ -31,6 +31,9 @@ import rospy
 import signal		# To handle Signals by OS/user
 import sys		# To handle Signals by OS/user
 
+import numpy as np
+import geometry_msgs.msg
+
 from geometry_msgs.msg import Wrench		# Message type used for publishing force vectors
 from geometry_msgs.msg import PoseArray	# Message type used for receiving goals
 from geometry_msgs.msg import Pose2D		# Message type used for receiving feedback
@@ -44,9 +47,9 @@ from tf.transformations import euler_from_quaternion	# Convert angles
 
 PI = 3.14
 
-x_goals = []
-y_goals = []
-theta_goals = []
+x_goals = [249,250,248,251,249]
+y_goals = [249,250,251,248,249]
+theta_goals = [0,PI/4,-3*PI/4,3*PI/4,0]
 
 right_wheel_pub = None
 left_wheel_pub = None
@@ -61,10 +64,10 @@ def signal_handler(sig, frame):
 	  
 	# NOTE: This function is called when a program is terminated by "Ctr+C" i.e. SIGINT signal 	
 	print('Clean-up !')
-	cleanup()
+	# cleanup()
 	sys.exit(0)
 
-def cleanup():
+# def cleanup():
 	############ ADD YOUR CODE HERE ############
 
 	# INSTRUCTIONS & HELP : 
@@ -74,24 +77,31 @@ def cleanup():
 	############################################
   
   
-def task2_goals_Cb(msg):
-	global x_goals, y_goals, theta_goals
-	x_goals.clear()
-	y_goals.clear()
-	theta_goals.clear()
+# def task2_goals_Cb(msg):
+# 	global x_goals, y_goals, theta_goals
+# 	x_goals.clear()
+# 	y_goals.clear()
+# 	theta_goals.clear()
 
-	for waypoint_pose in msg.poses:
-		x_goals.append(waypoint_pose.position.x)
-		y_goals.append(waypoint_pose.position.y)
+# 	for waypoint_pose in msg.poses:
+# 		x_goals.append(waypoint_pose.position.x)
+# 		y_goals.append(waypoint_pose.position.y)
 
-		orientation_q = waypoint_pose.orientation
-		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-		theta_goal = euler_from_quaternion (orientation_list)[2]
-		theta_goals.append(theta_goal)
+# 		orientation_q = waypoint_pose.orientation
+# 		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+# 		theta_goal = euler_from_quaternion (orientation_list)[2]
+# 		theta_goals.append(theta_goal)
 
 def aruco_feedback_Cb(msg):
 	############ ADD YOUR CODE HERE ############
+	x=msg.x
+	y=msg.y
+	theta=msg.theta
+	inverse_kinematics(x,y,theta)
 
+	
+	
+	# orientation_q = msg.pose.pose.orientation
 	# INSTRUCTIONS & HELP : 
 	#	-> Receive & store the feedback / coordinates found by aruco detection logic.
 	#	-> This feedback plays the same role as the 'Odometry' did in the previous task.
@@ -99,15 +109,74 @@ def aruco_feedback_Cb(msg):
 	############################################
 
 
-def inverse_kinematics():
+def inverse_kinematics(x,y,theta):
 	############ ADD YOUR CODE HERE ############
-
+	print("hello")
+	
+	conversion_factor=[[ -0.33 , 0.58 , 0.33],                                                          
+ 			           [ -0.33 , -0.58 , 0.33],                                                           
+ 				       [ 0.67 , 0 ,0.33]]
+	A=[x-249+1,y-249-1,theta]
+	B=np.dot(conversion_factor,A)
+	v1,v2,v3=B[0],B[1],B[2]
+	print(v1,v2,v3)
+	# right_wheel_pub.publish(-76.23)
+	# left_wheel_pub.publish(-76.23)
+	# front_wheel_pub.publish(154.77)
+	time.sleep(1)
 	# INSTRUCTIONS & HELP : 
 	#	-> Use the target velocity you calculated for the robot in previous task, and
 	#	Process it further to find what proportions of that effort should be given to 3 individuals wheels !!
 	#	Publish the calculated efforts to actuate robot by applying force vectors on provided topics
 	############################################
+def pid(self, error, const):
+        prop = error
+        self.intg = error + self.intg
+        diff = error - self.last_error
+        balance = const['Kp'] * prop + const['Ki'] * self.intg + const['Kd'] * diff
+        self.last_error = error
+        return balance
 
+    # angular pid function
+def getAngVel(self, error, const):
+        ang_vel=0
+
+        if abs(error) > self.ang_thresh:
+            if error > 3.14:
+                ang_vel = self.pid((error-6.28), const)
+            elif error < -3.14:
+                ang_vel = self.pid((error+6.28), const)
+            else:
+                ang_vel = self.pid(error, const)
+
+            if ang_vel<0: ang_vel=-1
+            else: ang_vel=1
+
+        else:
+            self.stop(z=True)
+
+        return ang_vel
+
+    # linear pid function
+def getLinearVel(self, error_x,  error_y, const, x=True):
+        v_x=0
+        v_y=0
+        
+        if abs(error_x)>self.linear_thresh or abs(error_y)>self.linear_thresh:
+            v_x=self.pid(error_x, const)
+            v_y=self.pid(error_y, const)
+        else:
+            self.stop(x=True, y=True)
+
+        return v_x, v_y
+
+    # bot halt function
+def stop(self, x=False, y=False, z=False):
+        if x: self.msg.linear.x = 0
+        if y: self.msg.linear.y= 0
+        if z: self.msg.angular.z = 0
+
+        self.cmd_pub.publish(self.msg)		
 
 def main():
 
@@ -120,13 +189,33 @@ def main():
 	right_wheel_pub = rospy.Publisher('/right_wheel_force', Wrench, queue_size=10)
 	front_wheel_pub = rospy.Publisher('/front_wheel_force', Wrench, queue_size=10)
 	left_wheel_pub = rospy.Publisher('/left_wheel_force', Wrench, queue_size=10)
-
-	rospy.Subscriber('detected_aruco',Pose2D,aruco_feedback_Cb)
-	rospy.Subscriber('task2_goals',PoseArray,task2_goals_Cb)
+	pose_msg = Wrench()
+	pose_msg.force.x = 0
+	pose_msg = geometry_msgs.msg.Wrench(force = geometry_msgs.msg.Vector3( x = 0, \
+                y = 0, z = 0), \
+                torque = geometry_msgs.msg.Vector3(x = 0, y = 0, z = 100))
+	# pose_msg = pose_msg.force.z=10
+	# pose_msg.force.x = 0
+	# pose_msg.force.y = 0
+	# pose_msg.torque.z = 10
+	right_wheel_pub.publish(pose_msg)
+	# left_wheel_pub.publish()
+	# front_wheel_pub.publish()
+	rospy.Subscriber('/detected_aruco',Pose2D,aruco_feedback_Cb)
+	# rospy.Subscriber('task2_goals',PoseArray,task2_goals_Cb)
 	
 	rate = rospy.Rate(100)
 
 	############ ADD YOUR CODE HERE ############
+	# pid params
+	params_linear={'Kp':1, 'Ki':0, 'Kd':0}
+    # params_ang={'Kp':0.5, 'Ki':0, 'Kd':0}
+    # intg=0
+    # last_error=0
+
+    # ROS msgs
+    # msg=Twist()
+    # rate=rospy.Rate(10)
 
 	# INSTRUCTIONS & HELP : 
 	#	-> Make use of the logic you have developed in previous task to go-to-goal.
@@ -137,11 +226,11 @@ def main():
 	#      given velocity of the chassis (Vx, Vy, W)
 	#	   
 
-		
+
 	while not rospy.is_shutdown():
 		
 		# Calculate Error from feedback
-
+		
 		# Change the frame by using Rotation Matrix (If you find it required)
 
 		# Calculate the required velocity of bot for the next iteration(s)
--- eyrc-2022_holabot/scripts/task 2/feedback.py
+++ eyrc-2022_holabot/scripts/task 2/feedback.py
@@ -1,72 +1,81 @@
 #!/usr/bin/env python3
 
-'''
-*****************************************************************************************
-*
-*        		===============================================
-*           		    HolA Bot (HB) Theme (eYRC 2022-23)
-*        		===============================================
-*
-*  This script should be used to implement Task 0 of HolA Bot (HB) Theme (eYRC 2022-23).
-*
-*  This software is made available on an "AS IS WHERE IS BASIS".
-*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
-*  any and all claim(s) that emanate from the use of the Software or
-*  breach of the terms of this agreement.
-*
-*****************************************************************************************
-'''
-
-# Team ID:		[ Team-ID ]
-# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
-# Filename:		feedback.py
-# Functions:
-#			[ Comma separated list of functions in this file ]
-# Nodes:		Add your publishing and subscribing node
-
-
-######################## IMPORT MODULES ##########################
-
-import numpy				# If you find it required
 import rospy 				
-from sensor_msgs.msg import Image 	# Image is the message type for images in ROS
-from cv_bridge import CvBridge	# Package to convert between ROS and OpenCV Images
-import cv2				# OpenCV Library
-import math				# If you find it required
-from geometry_msgs.msg import Pose2D	# Required to publish ARUCO's detected position & orientation
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2				
+import math	
+from geometry_msgs.msg import Pose2D
 
-############################ GLOBALS #############################
+class ArucoFeedback:
+	def __init__(self):
+		rospy.init_node('aruco_feedback_node')  
 
-aruco_publisher = rospy.Publisher('detected_aruco', Pose2D)
-aruco_msg = Pose2D()
+		self.bridge=CvBridge()
+		self.pose_msg = Pose2D()
 
-##################### FUNCTION DEFINITIONS #######################
+		# load the dictionary that was used to generate the markers
+		self.dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
 
-# NOTE :  You may define multiple helper functions here and use in your code
+		# initializing the detector parameters with default values
+		self.params =  cv2.aruco.DetectorParameters_create()
 
-def callback(data):
-	# Bridge is Used to Convert ROS Image message to OpenCV image
-	br = CvBridge()
-	rospy.loginfo("receiving camera frame")
-	get_frame = br.imgmsg_to_cv2(data, "mono8")		# Receiving raw image in a "grayscale" format
-	current_frame = cv2.resize(get_frame, (500, 500), interpolation = cv2.INTER_LINEAR)
+		# feed frame
+		self.frame=None
 
-	############ ADD YOUR CODE HERE ############
+		# pose
+		self.x=0
+		self.y=0
+		self.theta=0
+		
+		rospy.Subscriber('overhead_cam/image_raw', Image, self.callback)
+		self.pub = rospy.Publisher('detected_aruco', Pose2D, queue_size=10)
 
-	# INSTRUCTIONS & HELP : 
-	#	-> Use OpenCV to find ARUCO MARKER from the IMAGE
-	#	-> You are allowed to use any other library for ARUCO detection, 
-	#        but the code should be strictly written by your team and
-	#	   your code should take image & publish coordinates on the topics as specified only.  
-	#	-> Use basic high-school geometry of "TRAPEZOIDAL SHAPES" to find accurate marker coordinates & orientation :)
-	#	-> Observe the accuracy of aruco detection & handle every possible corner cases to get maximum scores !
+		while not rospy.is_shutdown():
+			if self.frame is None: continue
 
-	############################################
-      
-def main():
-	rospy.init_node('aruco_feedback_node')  
-	rospy.Subscriber('overhead_cam/image_raw', Image, callback)
-	rospy.spin()
-  
+			corners=self.detect_aruco(self.frame)
+			if len(corners)==0: continue
+
+			# store pose in the pose msg
+			self.pose_msg.x, self.pose_msg.y, self.pose_msg.theta = self.getPose(corners[0][0])
+			self.pub.publish(self.pose_msg)
+
+	def callback(self, data):
+		# recieves the feed
+		rospy.loginfo("receiving camera frame")
+		get_frame = self.bridge.imgmsg_to_cv2(data, "mono8")
+		self.frame = cv2.resize(get_frame, (500, 500), interpolation = cv2.INTER_LINEAR)
+
+	def detect_aruco(self, aruco_frame):
+        #detects the presence of aruco markers in the cam feed and returns the coordinates of the corners
+        # detect the markers in the frame
+		corners, _, _ = cv2.aruco.detectMarkers(aruco_frame, self.dict, parameters=self.params)
+
+		return corners
+
+	def getPose(self, corners):
+		# returns pose of the bot
+
+		x1, y1=corners[0] #upper left
+		x2, y2=corners[2] #lower right
+		x3, y3=corners[1] #upper right
+		x4, y4=corners[3] #lower left
+
+		#mid point of aruco marker
+		x, y=[int((x1+x2)/2), int((y1+y2)/2)]       
+		#mid point of right side of aruco marker  
+		x_rm, y_rm=[int((x2+x3)/2), int((y2+y3)/2)]   
+
+		# orientation
+		theta=math.atan2((y-y_rm), (x_rm-x))
+		
+		return x, y, theta
+
 if __name__ == '__main__':
-  main()
+    af=ArucoFeedback()
+    try:
+        if not rospy.is_shutdown():
+            rospy.spin()
+    except rospy.ROSInterruptException as e:
+        print(e)
